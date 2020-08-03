274ae8d9085c8393a83eae91ff3c03e78364ab0a
philip <giphilgolden@gmail.com>
2020-08-03 13:57:22 -0400

    test

---
+++ src/components/clocks/Clocks.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Welcome from "../welcome/Welcome";
+
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            date: new Date()
+        };
+    }
+
+    componentDidMount () {
+        this.timerId = setInterval(() => this.tick(), 1000)
+    }
+
+    componentWillUnmount () {
+        clearInterval(this.timerId)
+    }
+    tick () {
+        this.setState({
+            date: new Date()
+        })
+
+    }
+
+    render () {
+        return (
+            <div className="Clock">
+                {this.state.date.toLocaleTimeString()}
+            </div>
+        )
+    }
+
+}
+
+export default Clock;
